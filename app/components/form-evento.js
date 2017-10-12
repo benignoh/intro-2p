0f989ff824f0a87f7baff006fd3884daf113108f
Benigno Hernández H <bhernez@me.com>
2017-10-12 09:43:09 -0500

    * Nested form of Activities (1)

--- app/components/form-evento.js
+++ app/components/form-evento.js
@@ -17,13 +17,49 @@ export default Ember.Component.extend({
 				alert('El campo de nombre no puede estar vacio');
 				return;
 			}
+
+			if(this.get('actividadesArray')){
+				this.get('actividadesArray').forEach((a)=>{
+					a.set('evento', evento);
+				});
+			}
+
 			// 3) Mandar a guardar
 			evento.save().then(()=>{
 				// aquí ya estoy seguro que se guardó
-				alert('Ya se guardó');
-				this.sendAction('didSave');
+				if(this.get('actividadesArray')){
+					Ember.RSVP.all(this.get('actividadesArray').invoke('save')).then(()=>{
+						alert('Ya se guardó');
+						this.sendAction('didSave');
+					})
+				} else{
+					this.sendAction('didSave');
+				}
 			}); // es asíncrono
 			// es posible que aún no se haya guardado
+		},
+
+		saveActivity(){
+			// Inicializa una nueva activity en el store,
+			// y le llena sus atributos con las varibles del formulario
+			let activity = this.get('store').createRecord('activity', {
+				nombre: this.get('nombreActividad'),
+				inicio: this.get('inicioActividad'),
+				fin: this.get('finActividad')
+			});
+
+			// La nueva acivity creada la insertamos en un arreglo temporal
+			if(!Ember.isPresent(this.get('actividadesArray'))){
+				this.set('actividadesArray', [])
+			};
+			this.get('actividadesArray').pushObject(activity);
+
+			// Limpiamos (reset) las variables del fomrulario
+			this.setProperties({
+				nombreActividad: null,
+				inicioActividad: null,
+				finActividad: null
+			});
 		}
 	}
 });
